5fd075b8c895e522f5332c54b38e8ed42dc1d465
joao vitor de lima gonçalves <88281625+joaolima16@users.noreply.github.com>
2023-05-18 13:53:44 -0300

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace login_registro_asp.Controllers
+{
+    public class Login : Controller
+    {
+        private readonly ILogger<Login> _logger;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace login_registro_asp.Controllers
+{
+    public class Register : Controller
+    {
+        private readonly ILogger<Register> _logger;
+
+        public Register(ILogger<Register> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
